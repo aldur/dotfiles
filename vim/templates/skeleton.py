a84cabf3e189d4c6f018e46551763f57f95e7e6e
Adriano Di Luzio <adrianodl@hotmail.it>
2017-05-15 14:47:29 +0200

    Add vim templates

---
+++ vim/templates/skeleton.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""Module docstring."""
+
+__author__ = 'Adriano Di Luzio <adrianodl@hotmail.it>'
+
+
+def main():
+    """Docstring."""
+    pass
+
+
+if __name__ == "__main__":
+    main()
