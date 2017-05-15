d51ca1e5ea89eaccd940302c3dde2077ff6c461a
Adriano Di Luzio <adrianodl@hotmail.it>
2017-05-15 13:01:08 +0200

    Update python/pdb rc files

--- various/pdbrc.py
+++ various/pdbrc.py
@@ -1,13 +1,18 @@
-# encoding: utf-8
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 # vim:ft=python
+
+"""Enable history for pdb sessions."""
 
 import os.path
 import readline
 import atexit
 
-history_path = os.path.expanduser("~/.pdb_history")
+HISTORY_PATH = os.path.expanduser("~/.pdb_history")
 
-if os.path.exists(history_path):
-    readline.read_history_file(history_path)
-write_history = lambda: readline.write_history_file(history_path)
-atexit.register(write_history)
+
+try:
+    readline.read_history_file(HISTORY_PATH)
+except IOError:
+    pass
+atexit.register(readline.write_history_file, HISTORY_PATH)
