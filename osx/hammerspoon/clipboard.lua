da1930e2a8d204a68a765e378a820ca112fdd7a8
Adriano Di Luzio <adrianodl@hotmail.it>
2018-04-05 09:34:48 +0200

    Update archive size

--- osx/hammerspoon/clipboard.lua
+++ osx/hammerspoon/clipboard.lua
@@ -9,7 +9,7 @@ module = {
     frequency=1.0,  -- Clipboar polling frequency.
     chooser_max_size=1000,  -- Number of elements to store in the chooser.
     element_max_length=1000,  -- Maximum length of elements to store.
-    archive_max_size=10000,  -- Store additional n items in the 'archive'
+    archive_max_size=20000,  -- Store additional n items in the 'archive'
 
     trim=true,  -- Strip leading/trailing whitespace
 
