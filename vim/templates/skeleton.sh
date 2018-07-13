3705cbe8288c220c5d3249478249c82642fb7cf9
Adriano Di Luzio <adrianodl@hotmail.it>
2018-07-13 08:55:28 +0200

    Update sh skeleton

--- vim/templates/skeleton.sh
+++ vim/templates/skeleton.sh
@@ -1,9 +1,14 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-set -euo pipefail
-IFS=$'\n\t'
-
-
+# shellcheck disable=SC2016
+if test "$BASH" = "" || "$BASH" -uc 'a=();true "${a[@]}"' 2>/dev/null; then
+    # Bash 4.4, Zsh
+    set -euo pipefail
+else
+    # Bash 4.3 and older chokes on empty arrays with set -u.
+    set -eo pipefail
+fi
+shopt -s nullglob globstar
 
 finish() {
     true
