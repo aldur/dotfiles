4925dbfb5c13e68cbf12fc0a5c30b7fce172ad7b
Adriano Di Luzio <adrianodl@hotmail.it>
2019-10-15 10:30:18 -0400

    Switch to Apple music

--- osx/hammerspoon/init.lua
+++ osx/hammerspoon/init.lua
@@ -376,17 +376,17 @@ local function focusOrSwitch(bundleID)
     end
 end
 
-local spotifyBundleID = nil  -- Get the bundle ID of the spotify Chrome app
-local spotifyBundleInfo = hs.application.infoForBundlePath(
-    [[~/Applications/Chrome Apps.localized/Spotify.app]]
-)
-if spotifyBundleInfo ~= nil then
-    spotifyBundleID = spotifyBundleInfo['CFBundleIdentifier']
-end
+-- local spotifyBundleID = nil  -- Get the bundle ID of the spotify Chrome app
+-- local spotifyBundleInfo = hs.application.infoForBundlePath(
+--     [[~/Applications/Chrome Apps.localized/Spotify.app]]
+-- )
+-- if spotifyBundleInfo ~= nil then
+--     spotifyBundleID = spotifyBundleInfo['CFBundleIdentifier']
+-- end
 
 -- Focus/launch most commonly used applications.
 hs.fnutils.each({
-    {'M', spotifyBundleID}, {'B', 'com.google.Chrome'},
+    {'M', com.apple.Music}, {'B', 'com.google.Chrome'},
     {'W', 'com.kapeli.dashdoc'}, {'T', 'com.qvacua.VimR'},
 }, function(k)
     hs.hotkey.bind(hyper, k[1], function() focusOrSwitch(k[2]) end)
