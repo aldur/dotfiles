8a967aa72055d3fcf056ca88d60d969a112e7b30
Adriano Di Luzio <adrianodl@hotmail.it>
2020-04-06 17:01:37 +0200

    Do not allow others to reset input device statuses

--- osx/hammerspoon/audio.lua
+++ osx/hammerspoon/audio.lua
@@ -2,37 +2,83 @@
 -- luacheck: globals globals
 
 local logger = hs.logger.new('mic')
+-- logger.level = 3
 
 local module = {}
-module.is_muted = false
 module.__menubar = nil
 
-local function setAudioInput(is_muted)
+-- This is the single source of truth for this module.
+-- If this is set to true, then all input devices MUST be muted.
+module.isMuted = false
+
+local function setAudioInput(isMuted)
     for _, d in pairs(hs.audiodevice.allInputDevices()) do
         local name = d:name()
         if name == nil then name = '<nil>' end
-        if is_muted then
-            logger.d('Muting device ' .. name .. ' input.')
+        if isMuted then
+            logger.d('Muting input of ' .. name .. '.')
         else
-            logger.d('Unmuting device ' .. name .. ' input.')
+            logger.d('Unmuting input of ' .. name .. '.')
         end
-        d:setInputMuted(module.is_muted)
+        assert(d:setInputMuted(module.isMuted))
     end
 end
 
-function module.audiowatch(_)
-    -- When a audio device gets added or removed, we mute it as well.
-    setAudioInput(module.is_muted)
+local function setAudioWatch()
+    for _, d in pairs(hs.audiodevice.allInputDevices()) do
+        if not d:watcherIsRunning() then
+            assert(d:watcherCallback(module.localAudioWatchCallback))
+            assert(d:watcherStart())
+        end
+    end
+end
+
+function module.localAudioWatchCallback(device_uid, event_name, event_scope, _)
+    -- Some applications (I'm looking at you, Zoom US), override the mute-state.
+    -- If that happens, we reset it to the truth of this module.
+    if event_name == 'mute' and (event_scope == 'glob' or event_scope == 'inpt') then
+        local d = hs.audiodevice.findDeviceByUID(device_uid)
+        if d == nil then
+            logger.w('Received an event for `nil` device with name "' .. device_uid .. '".')
+        end
+
+        local name = d:name()
+        if name == nil then name = '<nil>' end
+
+        if d:inputMuted() ~= module.isMuted then
+            local m = 'false'
+            if module.isMuted then
+                m = 'true'
+            end
+            logger.i(
+                'Device named ' .. name ..' did not respect the module `isMuted` state (' .. m .. '). Re-setting it.'
+            )
+            assert(d:setInputMuted(module.isMuted))
+        end
+    end
+end
+
+-- Global audio device watcher. Triggered when a device gets added or removed.
+function module.globalAudioWatchCallback(arg)
+    logger.d('Received global event typed `' .. arg .. '`.')
+
+    if arg == 'dev#' then
+        -- A new device has been added or removed.
+        logger.d('Resetting audio state for all devices.')
+        setAudioInput(module.isMuted)
+        logger.d('Resetting audio watcher for all devices.')
+        setAudioWatch()
+    end
 end
 
 function module.toggleAudioInput()
-    module.is_muted = not module.is_muted
-    setAudioInput(module.is_muted)
+    module.isMuted = not module.isMuted
+    setAudioInput(module.isMuted)
     module.toggleMutedMenubar()
 end
 
 function module.toggleMutedMenubar()
-    if module.is_muted then
+    if module.isMuted then
         if module.__menubar ~= nil then return end
         module.__menubar = hs.menubar.new():setIcon('icons/audio-mute-on.pdf'):setTitle('muted'):setTooltip(
             'Audio Inputs Are Muted'):setMenu({{title='Audio Inputs Are Muted', disabled=true},
@@ -44,19 +90,25 @@ function module.toggleMutedMenubar()
     end
 end
 
-hs.audiodevice.watcher.setCallback(module.audiowatch)
-hs.audiodevice.watcher.start()
+hs.audiodevice.watcher.setCallback(module.globalAudioWatchCallback)
+if not hs.audiodevice.watcher.isRunning() then
+    hs.audiodevice.watcher.start()
+end
 
 local function start()
+    -- At startup, we use the `muted` state of the default device as truth.
     local device = hs.audiodevice.defaultInputDevice()
     if device == nil then return end
-    local is_muted = device:inputMuted()
-    if is_muted == nil then return end
+    local isMuted = device:inputMuted()
+    if isMuted == nil then return end
 
-    -- We set the local state to the opposite of `is_muted`,
+    -- Start the watcher on all connected devices.
+    setAudioWatch()
+
+    -- We set the local state to the opposite of `isMuted`,
     -- then we toggle the audio input so that we initialize it
     -- on all devices and we keep things consistent.
-    module.is_muted = not is_muted
+    module.isMuted = not isMuted
     module.toggleAudioInput()
 end
 start()
