0026236ef7a48414f5696c092ab4950612b99882
Adriano Di Luzio <adrianodl@hotmail.it>
2020-07-27 11:43:41 +0200

    Fix bash skeleton for old versions

--- vim/templates/skeleton.sh
+++ vim/templates/skeleton.sh
@@ -8,7 +8,9 @@ else
     # Bash 4.3 and older chokes on empty arrays with set -u.
     set -eo pipefail
 fi
-shopt -s nullglob globstar
+if shopt | grep globstar; then
+    shopt -s nullglob globstar || true
+fi
 
 finish() {
     true
