e6dbefdbabd8d22b85990b97b9d68f2f7bccc313
Adriano Di Luzio <adrianodl@hotmail.it>
2020-12-01 11:59:41 +0100

    Update Things plugin

--- vim/rplugin/python3/things_app.py
+++ vim/rplugin/python3/things_app.py
@@ -3,6 +3,8 @@
 
 """
 Push text snippets to Things 3 app.
+
+Inspired by: https://actions.getdrafts.com/a/1CO
 """
 
 import subprocess
@@ -12,19 +14,23 @@ import pynvim
 
 __author__ = "Adriano Di Luzio <adrianodl@hotmail.it>"
 
-THINGS_SCHEME = 'things:///'
-ADD_ACTION = 'add'
-QUICK_ENTRY = 'show-quick-entry'
+THINGS_SCHEME = 'things'
+VIMR_SCHEME = 'vimr'
+FILE_SCHEME = 'file'
+
+THINGS_ADD_ACTION = 'add'
+THINGS_QUICK_ENTRY = 'show-quick-entry'
+
+
+def prepare_uri(scheme, action, query_params=None):
+    query_params = query_params or {}
+    qs = urllib.parse.urlencode(query_params, True, quote_via=urllib.parse.quote)
+    return f"{scheme}:///{action}{'?' if qs else ''}{qs}"
 
 
 # Source: `vimr`
 def call_open(action, query_params):
-    query_params = query_params or {}
-    query_params.update({QUICK_ENTRY: True})
-    qs = urllib.parse.urlencode(query_params, True, quote_via=urllib.parse.quote)
-
-    url = f"{THINGS_SCHEME}{action}?{qs}"
-
+    url = prepare_uri(THINGS_SCHEME, action, query_params)
     subprocess.call(["/usr/bin/open", url])
     return url
 
@@ -36,18 +42,20 @@ class Things(object):
 
     @pynvim.command("ToThings3", range=True, nargs="*", sync=True)
     def command_handler(self, args, range):
+        buffer_path = self.nvim.current.buffer.name
         start, end = range
 
-        if end - start == 0:
-            self.nvim.err_write("No range provided...")
-            return -1
+        if not end - start:
+            title = buffer_path
+            notes = []
+        else:
+            title, *notes = self.nvim.current.buffer[start-1:end]
 
-        title, *lines = self.nvim.current.buffer[start-1:end]
-        kwargs = {'title': title}
-        if lines:
-            kwargs['notes'] = '\n'.join(lines)
+        file_uri = prepare_uri(FILE_SCHEME, buffer_path)
+        notes += ['\n', file_uri]
+        noets = '\n'.join(notes).rstrip()
+        kwargs = {'title': title, 'notes': notes}
+        kwargs.update({THINGS_QUICK_ENTRY: True})
 
-        url = call_open(ADD_ACTION, kwargs)
-
+        url = call_open(THINGS_ADD_ACTION, kwargs)
         self.nvim.err_write(url)
-        assert False, url
