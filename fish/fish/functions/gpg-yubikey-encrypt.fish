16d872f753ff92ce56335222baca8fe7d8bf9d21
Adriano Di Luzio <adrianodl@hotmail.it>
2021-05-05 09:04:53 +0200

    Add encryption function helpers

--- fish/fish/functions/gpg-yubikey-encrypt.fish
+++ fish/fish/functions/gpg-yubikey-encrypt.fish
@@ -1,5 +1,13 @@
+set yubikey_main '2D8785CE6E3B69AB'
+set yubikey_back 'FADB481FA744FFDF'
+set gpg_offline  '347B2D89DDDF596B'
+
 function gpg-yubikey-encrypt
-    # Encrypts with the GPG keys stored within your GPG.
-    gpg --encrypt --no-throw-keyids --armor --output $argv.gpg --recipient 2D8785CE6E3B69AB --recipient FADB481FA744FFDF $argv
+    # Encrypts with the GPG keys stored within your Yubikeys.
+    gpg --encrypt --no-throw-keyids --armor --output $argv.gpg --recipient $yubikey_main --recipient $yubikey_back --recipient $gpg_offline $argv
 end
 
+function gpg-yubikey-encrypt-pipe
+    # Encrypts with the GPG keys stored within your Yubikeys.
+    gpg --encrypt --no-throw-keyids --armor --recipient $yubikey_main --recipient $yubikey_back --recipient $gpg_offline
+end
