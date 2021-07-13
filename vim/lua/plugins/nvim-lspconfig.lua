0b39da164ff86f4e4c44296935965f35636adab0
Adriano Di Luzio <adrianodl@hotmail.it>
2021-07-13 11:59:24 +0200

    Integrate pipenv and pyright

--- vim/lua/plugins/nvim-lspconfig.lua
+++ vim/lua/plugins/nvim-lspconfig.lua
@@ -1,7 +1,31 @@
 -- Original credits to https://github.com/tomaskallup/dotfiles/blob/master/nvim/lua/plugins/nvim-lspconfig.lua
 local lspconfig = require 'lspconfig'
--- -- Make sure this is a slash (as theres some metamagic happening behind the scenes)
--- local configs = require("lspconfig/configs")
+local util = require('lspconfig/util')
+
+local path = util.path
+
+-- https://github.com/neovim/nvim-lspconfig/issues/500#issuecomment-876700701
+local function get_python_path(workspace)
+    -- Use activated virtualenv.
+    if vim.env.VIRTUAL_ENV then
+        return path.join(vim.env.VIRTUAL_ENV, 'bin', 'python')
+    end
+
+    -- Find and use virtualenv from pipenv in workspace directory.
+    local match = vim.fn.glob(path.join(workspace, 'Pipfile'))
+    if match ~= '' then
+        local venv = vim.fn.trim(vim.fn.system(
+                                     'PIPENV_PIPFILE=' .. match ..
+                                         ' pipenv --venv'))
+        local msg = "Activating Pipenv at " .. venv
+        vim.cmd(([[echohl Function | echomsg "%s" | echohl None]]):format(vim.fn.escape(msg, "\"\\")))
+
+        return path.join(venv, 'bin', 'python')
+    end
+
+    -- Fallback to system Python.
+    return vim.fn.exepath('python3') or vim.fn.exepath('python') or 'python'
+end
 
 -- Setup everything on lsp attach
 local on_attach = function(_, bufnr)
@@ -17,10 +41,10 @@ local on_attach = function(_, bufnr)
         vim.api.nvim_buf_set_keymap(bufnr, ...)
     end
 
-    -- Mnemonic for usages
+    -- Mnemonic for Usages
     buf_set_keymap('n', '<leader>u', '<cmd>lua vim.lsp.buf.references()<CR>',
                    opts)
-    -- Mnemonic for info
+    -- Mnemonic for Info
     buf_set_keymap('n', '<leader>i', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
 
     buf_set_keymap('n', '<leader>f', '<cmd>lua vim.lsp.buf.formatting()<CR>',
@@ -29,7 +53,14 @@ local on_attach = function(_, bufnr)
 end
 
 -- Python pyright
-lspconfig.pyright.setup {on_attach = on_attach}
+lspconfig.pyright.setup {}
+lspconfig.pyright.setup {
+    on_attach = on_attach,
+    on_init = function(client)
+        client.config.settings.python.pythonPath =
+            get_python_path(client.config.root_dir)
+    end
+}
 
 -- Vim lsp
 lspconfig.vimls.setup {on_attach = on_attach}
