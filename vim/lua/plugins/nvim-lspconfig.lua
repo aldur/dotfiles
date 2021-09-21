82cfdc1547360b9af6fc137ddab8c3c92df8e64a
Adriano Di Luzio <adrianodl@hotmail.it>
2021-09-21 11:00:48 +0200

    Disable formatting for pylsp

--- vim/lua/plugins/nvim-lspconfig.lua
+++ vim/lua/plugins/nvim-lspconfig.lua
@@ -80,7 +80,11 @@ end
 
 -- https://github.com/python-lsp/python-lsp-server
 lspconfig.pylsp.setup {
-    on_attach = on_attach,
+    on_attach = function(client, bufnr)
+        -- Disable formatting as we do it throuhg Black/efm.
+        client.resolved_capabilities.document_formatting = false
+        return on_attach(client, bufnr)
+    end,
     on_new_config = function(new_config, new_root_dir)
         new_config['cmd_env'] = pylps_cmd_env(new_root_dir)
     end
