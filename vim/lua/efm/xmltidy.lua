d2be6d1f570433117bb9db4f9613728002b56bdf
Adriano Di Luzio <adrianodl@hotmail.it>
2021-10-12 23:33:26 +0200

    Do not wrap `xml` with `tidy`

--- vim/lua/efm/xmltidy.lua
+++ vim/lua/efm/xmltidy.lua
@@ -1,1 +1,1 @@
-return {formatCommand = "tidy --indent yes --indent-spaces 4 --indent-attributes yes --wrap-attributes yes --input-xml yes --output-xml yes", formatStdin = true}
+return {formatCommand = "tidy --indent yes --indent-spaces 4 --indent-attributes yes --wrap-attributes yes --input-xml yes --output-xml yes --wrap 0", formatStdin = true}
