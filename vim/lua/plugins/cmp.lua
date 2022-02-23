d8402b6340c9ba21cec71d5b540e1ae3b5bdfa0c
Adriano Di Luzio <adrianodl@hotmail.it>
2022-02-23 10:40:26 +0100

    Suggest from all buffers

--- vim/lua/plugins/cmp.lua
+++ vim/lua/plugins/cmp.lua
@@ -110,13 +110,13 @@ cmp.setup({
             name = 'buffer',
             -- https://github.com/hrsh7th/cmp-buffer
             -- Source from visibile buffers.
-            get_bufnrs = function()
-                local bufs = {}
-                for _, win in ipairs(vim.api.nvim_list_wins()) do
-                    bufs[vim.api.nvim_win_get_buf(win)] = true
-                end
-                return vim.tbl_keys(bufs)
-            end
+            -- get_bufnrs = function()
+            --     local bufs = {}
+            --     for _, win in ipairs(vim.api.nvim_list_wins()) do
+            --         bufs[vim.api.nvim_win_get_buf(win)] = true
+            --     end
+            --     return vim.tbl_keys(bufs)
+            -- end
         }, {name = 'path'}
     }
 })
