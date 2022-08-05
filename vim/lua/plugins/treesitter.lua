7a3276502896c31b9b629e294c0730fed30a947f
Adriano Di Luzio <adrianodl@hotmail.it>
2022-08-05 16:52:53 +0200

    Disable phpdoc

--- vim/lua/plugins/treesitter.lua
+++ vim/lua/plugins/treesitter.lua
@@ -1,6 +1,7 @@
 -- https://github.com/nvim-treesitter/nvim-treesitter#modules
 require'nvim-treesitter.configs'.setup {
     ensure_installed = "all",
+    ignore_install = {"phpdoc"},
     highlight = {enable = true},
     indent = {enable = true, disable = {"python"}},
     textsubjects = {
