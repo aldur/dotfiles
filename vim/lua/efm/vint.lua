ce0e0e7fa6e2e0c1eee5585eb05b0aec04ad6ffd
Adriano Di Luzio <adrianodl@hotmail.it>
2023-03-09 23:49:08 +0100

    Enable neovim and report style problems for vint

--- vim/lua/efm/vint.lua
+++ vim/lua/efm/vint.lua
@@ -1,6 +1,6 @@
 -- https://github.com/lukas-reineke/dotfiles/blob/master/vim/lua/efm/vint.lua
 return {
-    lintCommand = "vint -",
+    lintCommand = "vint --enable-neovim --style-problem -",
     lintStdin = true,
     lintFormats = {"%f:%l:%c: %m"},
     lintSource = "vint"
