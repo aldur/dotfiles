f514dcedebda7b2d3d9056a57223f28ae8a6e75c
Adriano Di Luzio <adrianodl@hotmail.it>
2023-04-26 18:50:32 +0200

    Ignore luacheck exit code

--- vim/lua/efm/luacheck.lua
+++ vim/lua/efm/luacheck.lua
@@ -1,5 +1,6 @@
 return {
     lintCommand = "luacheck - --read-globals vim --formatter plain --codes --filename ${INPUT}",
     lintStdin = true,
+    lintIgnoreExitCode = true,
     lintFormats = {'%f:%l:%c: %m'}
 }
