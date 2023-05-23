eca4daac74048b989e082c0655cf28a7d63c6910
Adriano Di Luzio <adrianodl@hotmail.it>
2023-05-23 20:03:23 +0200

    Rename `notebook` to `container`

--- osx/scripts/process_remarkable_page.2.sh
+++ osx/scripts/process_remarkable_page.2.sh
@@ -38,13 +38,13 @@ if [[ $input_file_path == *"/.stversions/"* ]]; then
 	exit 0
 fi
 
-notebook_file=$(dirname "$input_file_path")
-notebook_metadata="$notebook_file".metadata
-notebook_name=$(jq -r '.visibleName' <"$notebook_metadata")
-notebook_name="${notebook_name//\//}" # Strip slashes in notebook name
+container_file=$(dirname "$input_file_path")
+container_metadata="$container_file".metadata
+container_name=$(jq -r '.visibleName' <"$container_metadata")
+container_name="${container_name//\//}" # Strip slashes in container name
 
 output="$output"/"$(date -ur "$input_file_path" "+%Y-%m-%d")"
-output="$output"/"$notebook_name"
+output="$output"/"$container_name"
 mkdir -p "$output"
 
 # We use this as a base not to create files within the original directory.
@@ -64,18 +64,18 @@ touch -r "$input_file_path" "$ocr_output"
 # Add to PDF as attachment/embedded file.
 qpdf "$full_output_no_ext.pdf" --add-attachment "$ocr_output" --mimetype=text/plain --description="OCR, Apple Handwriting Recognition" -- --replace-input
 
-notebook_content="$notebook_file".content
+container_content="$container_file".content
 
 # If it's a PDF, redirect the page to the actual page (in case of addition, deletion).
 # Otherwise, get the page index.
 # If not found, fallback to -1.
-page_index=$(jq '(.pages | index("'"$page_uid"'") // -2) + 1' "$notebook_content")
-pdf_page_index=$(jq '(.pageIndex = (.pages | index("'"$page_uid"'")) | (if has("redirectionPageMap") then .redirectionPageMap[.pageIndex] else null end) // -2) + 1' "$notebook_content")
+page_index=$(jq '(.pages | index("'"$page_uid"'") // -2) + 1' "$container_content")
+pdf_page_index=$(jq '(.pageIndex = (.pages | index("'"$page_uid"'")) | (if has("redirectionPageMap") then .redirectionPageMap[.pageIndex] else null end) // -2) + 1' "$container_content")
 
-notebook_uid=$(basename "$notebook_file")
-full_output_with_page="$output"/"$page_index"."$notebook_uid"."$page_uid"
+container_uuid=$(basename "$container_file")
+full_output_with_page="$output"/"$page_index"."$container_uuid"."$page_uid"
 
-pdf_file="$notebook_file".pdf
+pdf_file="$container_file".pdf
 if [[ -f $pdf_file ]] && [[ $page_index -gt 0 ]] && [[ $pdf_page_index -gt 0 ]]; then
 	qpdf --empty --pages "$pdf_file" "$pdf_page_index" -- "$full_output_with_page".pdf
 	qpdf --replace-input --overlay "$full_output_no_ext".pdf -- "$full_output_with_page".pdf
@@ -84,11 +84,11 @@ else
 	mv "$full_output_no_ext.pdf" "$full_output_with_page".pdf
 fi
 
-ocr_output_with_page="$output"/"ocr"/"$page_index"."$notebook_uid"."$page_uid".ocr.txt
+ocr_output_with_page="$output"/"ocr"/"$page_index"."$container_uuid"."$page_uid".ocr.txt
 mv "$ocr_output" "$ocr_output_with_page"
 touch -r "$input_file_path" "$full_output_with_page".pdf
 touch -r "$input_file_path" "$ocr_output_with_page"
 
-full_notebook="$output"/../"$notebook_name".pdf
-qpdf --empty --pages "$output"/*."$notebook_uid".*.pdf -- "$full_notebook"
+full_container="$output"/../"$container_name".pdf
+qpdf --empty --pages "$output"/*."$container_uuid".*.pdf -- "$full_container"
 # TODO: Add attachments / OCRs to merged PDF.
