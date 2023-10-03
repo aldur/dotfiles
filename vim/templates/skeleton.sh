6e3d07a8ad246463361bb33c6a46ea96cbd0b69d
Adriano Di Luzio <adrianodl@hotmail.it>
2023-10-03 22:17:00 +0200

    Reformat

--- vim/templates/skeleton.sh
+++ vim/templates/skeleton.sh
@@ -2,18 +2,18 @@
 
 # shellcheck disable=SC2016
 if test "$BASH" = "" || "$BASH" -uc 'a=();true "${a[@]}"' 2>/dev/null; then
-    # Bash 4.4, Zsh
-    set -Eeuo pipefail
+	# Bash 4.4, Zsh
+	set -Eeuo pipefail
 else
-    # Bash 4.3 and older chokes on empty arrays with set -u.
-    set -Eeo pipefail
+	# Bash 4.3 and older chokes on empty arrays with set -u.
+	set -Eeo pipefail
 fi
-if shopt | grep globstar; then
-    shopt -s nullglob globstar || true
+if shopt | grep globstar &>/dev/null; then
+	shopt -s nullglob globstar || true
 fi
 
 trap cleanup SIGINT SIGTERM ERR EXIT
 
 cleanup() {
-    trap - SIGINT SIGTERM ERR EXIT
+	trap - SIGINT SIGTERM ERR EXIT
 }
