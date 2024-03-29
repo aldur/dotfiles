67d2bf64805b36f26408ac50b099410c2fc46a5e
Adriano Di Luzio <adrianodl@hotmail.it>
2024-03-29 17:12:34 +0100

    Fix typo

--- vim/lua/plugins/beancount.lua
+++ vim/lua/plugins/beancount.lua
@@ -6,7 +6,7 @@ function M.reload_beancount_completions()
     for _, s in pairs(cmp.core.sources) do
         if s.name == "beancount" then
             s.source.items = nil
-            print("Beacnount completions will reload...")
+            print("Beancount completions will reload...")
         end
     end
 end
