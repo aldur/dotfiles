d419fbe6e1b13eef2538a3633bee5dee0155be1c
Adriano Di Luzio <adrianodl@hotmail.it>
2024-03-29 17:11:18 +0100

    Add one more yubikey

--- fish/fish/functions/gpg-yubikey-encrypt-pipe.fish
+++ fish/fish/functions/gpg-yubikey-encrypt-pipe.fish
@@ -2,5 +2,5 @@ source ~/.config/fish/functions/gpg_keys.fish
 
 function gpg-yubikey-encrypt-pipe
     # Encrypts with the GPG keys stored within your Yubikeys.
-    gpg --encrypt --no-throw-keyids --recipient $yubikey_main --recipient $yubikey_back --recipient $gpg_offline
+    gpg --encrypt --no-throw-keyids --recipient $yubikey_main --recipient $yubikey_back --recipient $gpg_offline --recipient $yubikey_milan
 end
--- fish/fish/functions/gpg_keys.fish
+++ fish/fish/functions/gpg_keys.fish
@@ -1,3 +1,4 @@
 set yubikey_main '2D8785CE6E3B69AB'
 set yubikey_back 'FADB481FA744FFDF'
-set gpg_offline  '347B2D89DDDF596B'
+set yubikey_milan 'FADB481FA744FFDF'
+set gpg_offline  '3D05D1D4F4FC6077'
