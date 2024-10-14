14ea0eb9583f19deae649efeab9a356d4ca9f3f4
Adriano Di Luzio <adrianodl@hotmail.it>
2024-10-14 10:28:50 +0200

    Needs to be on same line

--- fish/fish/functions/flakify.fish
+++ fish/fish/functions/flakify.fish
@@ -2,8 +2,7 @@ function flakify
     if not test -e flake.nix
         nix flake new -t github:nix-community/nix-direnv .
     else if not test -f .envrc
-        echo "use flake" > .envrc
-        direnv allow
+        echo "use flake" > .envrc && direnv allow
     end
     exec $EDITOR flake.nix
 end
