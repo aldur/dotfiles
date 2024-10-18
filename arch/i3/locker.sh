cd2022665665bfebdd02b7393242fddb8a6ef46a
Adriano Di Luzio <adrianodl@hotmail.it>
2024-10-18 15:15:17 +0100

    Remove `arch`

--- arch/i3/locker.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-exec xautolock -detectsleep 
-  -time 5 -locker "i3lock-wrapper -d" \
-  -notify 30 \
-  -notifier "notify-send -u critical -t 10000 -- 'LOCKING screen in 30 seconds'"
-
-exec xautolock -time 10 -locker "systemctl suspend" -detectsleep
