ddba243fed2aff7d8ce6ab1915390645bf77a52a
Adriano Di Luzio <adrianodl@hotmail.it>
2024-10-18 17:56:12 +0100

    Can't sandbox (neovim build breaks its max size)

--- osx/flake.nix
+++ osx/flake.nix
@@ -21,6 +21,7 @@
 
           nix.settings.allowed-users = [ user ];
           nix.settings.trusted-users = [ "root" ];
+          nix.settings.sandbox = false;
 
           # Necessary for using flakes on this system.
           nix.settings.experimental-features = "nix-command flakes";
