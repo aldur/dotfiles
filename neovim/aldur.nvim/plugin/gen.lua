de28d264767fc703554c4d2e13d20912138489d0
Adriano Di Luzio <adrianodl@hotmail.it>
2024-10-18 20:54:42 +0100

    Configure `gen.nvim` directly

---
+++ neovim/aldur.nvim/plugin/gen.lua
@@ -0,0 +1,58 @@
+require('gen').setup({
+    model = "nemotron",
+    show_prompt = true,
+    show_model = true,
+    no_auto_close = true,
+    debug = false,
+    display_mode = "split"
+})
+
+vim.keymap.set({'n', 'v'}, '<leader>g', '<cmd>Gen <CR>', {noremap = true})
+
+require('gen').prompts['Links_to_References'] = {
+    prompt = [[
+        Answer ONLY with the result of your task that follows. No additional
+        text, no explanation.
+
+        --- Instructions:
+
+        Rewrite the following Markdown by converting any link into references
+        syntax.
+
+        --- Example:
+
+        This is a [very long link](https://example.com/foo/bar) where I tell
+        you something.
+
+        --- Example result:
+
+        This is a [very long link][example_link] where I tell you something.
+
+        [example_link]: https://example.com/foo/bar
+
+        --- Your task:
+
+        $text
+    ]],
+    replace = true
+}
+
+require('gen').prompts['Explain_Code'] = {
+    prompt = [[
+        Explain the following code, line by line.
+
+        ```$filetype
+        $text
+        ```
+    ]]
+}
+
+require('gen').prompts['Ask'] = {
+    prompt = [[
+        Regarding the following text, $input:
+
+        ```$filetype
+        $text
+        ```
+    ]]
+}
--- neovim/aldur.nvim/plugin/gen.vim
+++
@@ -1,4 +0,0 @@
-nnoremap <leader>g :Gen<cr>
-vnoremap <leader>g :Gen<cr>
-
-lua require('aldur.gen')
