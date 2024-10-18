5a5b8fe8b20c97bccfdd0469d3351f4de7e8e845
Adriano Di Luzio <adrianodl@hotmail.it>
2024-10-18 18:27:53 +0100

    Make patching work with `nix` neovide

--- osx/scripts/patch_neovide.sh
+++ osx/scripts/patch_neovide.sh
@@ -12,57 +12,58 @@ if shopt | grep globstar; then
 	shopt -s nullglob globstar || true
 fi
 
+set -x
+
 # Magic script to get this script's directory.
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
-NEOVIDE_APP=/Applications/Neovide.app
-NEOVIDE_PLIST=$NEOVIDE_APP/Contents/Info.plist
-NEOVIDE_EXECUTABLE=$NEOVIDE_APP/Contents/MacOS/neovide
+
+NEOVIDE_APP="/Applications/Nix Apps/Neovide.app"
+if test ! -e "$NEOVIDE_APP"; then
+	NEOVIDE_APP="/Applications/Neovide.app"
+	if test ! -e "$NEOVIDE_APP"; then
+		echo "Can't find Neovide.app!"
+		exit 1
+	fi
+fi
+
+NEOVIDE_PLIST="$NEOVIDE_APP"/Contents/Info.plist
+NEOVIDE_EXECUTABLE="$NEOVIDE_APP"/Contents/MacOS/neovide
 NEOVIDE_SERVER="$NEOVIDE_EXECUTABLE"_server.sh
 
-if test ! -f $NEOVIDE_PLIST; then
+if test ! -f "$NEOVIDE_PLIST"; then
 	echo "Can't find Neovide's plist!"
 	exit 1
 fi
 
-# Ensure's homebrew's PATH
-PATH=/opt/homebrew/bin:$PATH
-
-if NEOVIDE_PATH=$(which neovide); then
-	if ! test -L "$NEOVIDE_PATH"; then
-		echo "Neovide is on PATH, but is not a symlink!"
-		exit 1
-	fi
-else
-	NEOVIDE_PATH=$(brew --prefix)/bin/neovide
-fi
-
 NVIM_SOCKET=/tmp/neovide.socket
+NVIM_BINARY=/Users/aldur/.dotfiles/neovim/result/bin/nvim
 
-cat <<EOF >$NEOVIDE_SERVER
+cat <<EOF >"$NEOVIDE_SERVER"
 #!/bin/sh
-
-# Needed for realpath
-PATH=/opt/homebrew/opt/coreutils/libexec/gnubin/realpath:$PATH
-export PATH
 
 NVIM_SOCKET=$NVIM_SOCKET
 if [ ! -S \$NVIM_SOCKET ]; then
-    $NEOVIDE_EXECUTABLE --frame=transparent --title-hidden -- --listen \$NVIM_SOCKET --cmd "cd ~" "\$@"
+    "$NEOVIDE_EXECUTABLE" --frame=transparent --title-hidden --neovim-bin "$NVIM_BINARY" -- --listen \$NVIM_SOCKET --cmd "cd ~" "\$@"
 else
-    nvim --server \$NVIM_SOCKET --remote "\$(/opt/homebrew/opt/coreutils/libexec/gnubin/realpath -m "\$@" | tr '\n' ' ' | sed 's/ $/\n/')"
+    "$NVIM_BINARY" --server \$NVIM_SOCKET --remote "\$(grealpath -m "\$@" | tr '\n' ' ' | sed 's/ $/\n/')"
     open -a Neovide  # Ensures front
 fi
 EOF
 
-chmod +x $NEOVIDE_SERVER
+chmod +x "$NEOVIDE_SERVER"
 
-ln -sf $NEOVIDE_SERVER "$NEOVIDE_PATH"
+# If '`neovide` is on PATH, we replace it with patched version
+if NEOVIDE_PATH=$(which neovide); then
+	if test -L "$NEOVIDE_PATH"; then
+		ln -sf "$NEOVIDE_SERVER" "$NEOVIDE_PATH"
+	fi
+fi
 
-cp $NEOVIDE_PLIST $NEOVIDE_PLIST.back
+cp "$NEOVIDE_PLIST" "$NEOVIDE_PLIST.back"
 # https://www.marcosantadev.com/manage-plist-files-plistbuddy/
-/usr/libexec/PlistBuddy -c "Set CFBundleExecutable $(basename $NEOVIDE_SERVER)" $NEOVIDE_PLIST
+/usr/libexec/PlistBuddy -c "Set CFBundleExecutable $(basename "$NEOVIDE_SERVER")" "$NEOVIDE_PLIST"
 
-cp "$SCRIPT_DIR"/neovide.icns $NEOVIDE_APP/Contents/Resources/neovide.icns
+cp "$SCRIPT_DIR"/neovide.icns "$NEOVIDE_APP/Contents/Resources/neovide.icns"
 
 # https://superuser.com/questions/271678/how-do-i-pass-command-line-arguments-to-dock-items
-/System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister -f $NEOVIDE_APP
+/System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister -f "$NEOVIDE_APP"
