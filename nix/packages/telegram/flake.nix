91081cb9784d13bd348ac0b712af4541da16a630
Adriano Di Luzio <adrianodl@hotmail.it>
2024-11-04 19:51:04 +0000

    Fix `curl` unavailable at runtime

--- nix/packages/telegram/flake.nix
+++ nix/packages/telegram/flake.nix
@@ -11,28 +11,34 @@
   outputs =
     { nixpkgs, flake-utils, ... }:
     let
-      telegram = pkgs: pkgs.stdenv.mkDerivation {
-        name = "telegram";
-        nativeBuildInputs = [
-          pkgs.curl
-        ];
-        src = ./telegram.sh;
-        dontUnpack = true;
-        buildPhase = ''
-          install -Dm755 ${./telegram.sh} $out/bin/telegram
-        '';
-      };
+      telegram =
+        pkgs:
+        pkgs.stdenv.mkDerivation rec {
+          name = "telegram";
+          nativeBuildInputs = [
+            pkgs.makeWrapper
+          ];
+          buildIputs = [
+            pkgs.curl
+          ];
+          src = ./telegram.sh;
+          dontUnpack = true;
+          buildPhase = ''
+            install -Dm755 ${./telegram.sh} $out/bin/telegram
+          '';
+          postFixup = ''
+            wrapProgram $out/bin/telegram \
+              --set PATH ${pkgs.lib.makeBinPath buildIputs}
+          '';
+        };
     in
-    flake-utils.lib.eachDefaultSystem
-      (
-        system:
-        rec {
-          legacyPackages = import nixpkgs {
-            inherit system;
-          };
-          packages.default = telegram legacyPackages;
-        }
-      ) // {
+    flake-utils.lib.eachDefaultSystem (system: rec {
+      legacyPackages = import nixpkgs {
+        inherit system;
+      };
+      packages.default = telegram legacyPackages;
+    })
+    // {
       overlays.default = final: prev: {
         count-tokens = (telegram prev);
       };
