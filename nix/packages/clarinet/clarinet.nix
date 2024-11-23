7622c2032b234fd137d01505b67c95ca7007c190
Adriano Di Luzio <adrianodl@hotmail.it>
2024-11-23 20:25:29 +0000

    Make clarinet compatible with Linux

--- nix/packages/clarinet/clarinet.nix
+++ nix/packages/clarinet/clarinet.nix
@@ -1,27 +1,43 @@
-{ fetchzip, stdenv }:
+{
+  fetchzip,
+  stdenv,
+  system,
+}:
 {
   clarinet = stdenv.mkDerivation rec {
     pname = "clarinet";
     version = "v2.11.2";
 
-    src = fetchzip {
-      # FIXME: This only works on macOS / arm64
-      # https://github.com/hirosystems/clarinet/releases/download/v2.11.2/clarinet-darwin-arm64.tar.gz
-      url = "https://github.com/hirosystems/${pname}/releases/download/${version}/${pname}-darwin-arm64.tar.gz";
-      hash = "sha256-fG3oKBt780NAI5wNh49lO7YumbRYigBHtRVAjaIfKUc=";
-    };
+    src =
+      let
+        architecture_and_hash =
+          {
+            aarch64-darwin = [
+              "darwin-arm64"
+              "sha256-fG3oKBt780NAI5wNh49lO7YumbRYigBHtRVAjaIfKUc="
+            ];
+            x86_64-linux = [
+              "linux-x64-glibc"
+              "sha256-ivbTcJBrzwgu2mLAoJqjqWqNMXGdsYiDQ7wgs22U7AU="
+            ];
+            x86_64-darwin = [
+              "darwin-x64"
+              "sha256-zjUvRPLN08PQAcX9B44FbITgjaKNQZsLCJgUsr54358="
+            ];
+          }
+          .${system};
+      in
+      fetchzip {
+        url = "https://github.com/hirosystems/${pname}/releases/download/${version}/${pname}-${
+          builtins.elemAt architecture_and_hash 0
+        }.tar.gz";
+        hash = builtins.elemAt architecture_and_hash 1;
+      };
 
     phases = [ "installPhase" ];
     installPhase = ''
       runHook preInstall
-
-      if [[ "$(uname)" != "Darwin" ]] || [[ "$(uname -m)" != "arm64" ]]; then
-        echo "This derivation only implements downloads for macOS with an arm64 architecture."
-        exit 1
-      fi
-
       install -Dm755 $src/clarinet $out/bin/clarinet
-
       runHook postInstall
     '';
   };
