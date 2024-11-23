198d9b1ba053bdd939314a2565d3f11d0180894c
Adriano Di Luzio <adrianodl@hotmail.it>
2024-11-23 20:26:24 +0000

    Add mapping to quickly add to LLM buffer

--- neovim/aldur.nvim/plugin/llm.lua
+++ neovim/aldur.nvim/plugin/llm.lua
@@ -1,8 +1,10 @@
 local function ollama_model(name)
-    return require("codecompanion.adapters").extend("ollama", {
-        name = name,
-        schema = {model = {default = name}, num_predict = {default = -1}}
-    })
+    return function()
+        return require("codecompanion.adapters").extend("ollama", {
+            name = name,
+            schema = {model = {default = name}, num_predict = {default = -1}}
+        })
+    end
 end
 
 require("codecompanion").setup({
@@ -38,8 +40,8 @@ vim.api.nvim_set_keymap("n", "<LocalLeader>a",
 vim.api.nvim_set_keymap("v", "<LocalLeader>a",
                         "<cmd>CodeCompanionChat Toggle<cr>",
                         {noremap = true, silent = true})
--- vim.api.nvim_set_keymap("v", "ga", "<cmd>CodeCompanionChat Add<cr>",
---                         {noremap = true, silent = true})
+vim.api.nvim_set_keymap("v", "ga", "<cmd>CodeCompanionChat Add<cr>",
+                        {noremap = true, silent = true})
 
 -- Expand 'cc' into 'CodeCompanion' in the command line
 vim.cmd([[cab cc CodeCompanion]])
