608c70fadd553668d17a22d2813e057942162640
Adriano Di Luzio <aldur@users.noreply.github.com>
2025-10-15 19:37:17 +0200

    Create an empty direnvrc

--- nix/modules/direnv.nix
+++ nix/modules/direnv.nix
@@ -1,10 +1,14 @@
-{ ... }:
-{
+{ ... }: {
   programs.direnv.enable = true;
   programs.direnv.nix-direnv.enable = true;
 
   environment.variables = {
     # https://esham.io/2023/10/direnv
     DIRENV_LOG_FORMAT = ''$(printf "\033[2mdirenv: %%s\033[0m")'';
+  };
+
+  # https://github.com/direnv/direnv/issues/1418
+  home-manager.users.aldur = { ... }: {
+    home.file.".config/direnv/direnvrc".text = "";
   };
 }
