66b74609a175b1011e12263f5f4e30260dec91f0
Adriano Di Luzio <aldur@users.noreply.github.com>
2025-10-20 10:41:49 +0200

    Fix direnv toml

--- nix/modules/direnv.nix
+++ nix/modules/direnv.nix
@@ -9,6 +9,6 @@
 
   # https://github.com/direnv/direnv/issues/1418
   home-manager.users.aldur = { ... }: {
-    home.file.".config/direnv/direnvrc".text = "";
+    home.file.".config/direnv/direnv.toml".text = "";
   };
 }
