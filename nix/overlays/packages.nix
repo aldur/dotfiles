d5bb638c061ef93821cc31d08ce414578aa4dd2b
Adriano Di Luzio <aldur@users.noreply.github.com>
2025-10-21 17:34:46 +0200

    Update `packages.nix`

--- nix/overlays/packages.nix
+++ nix/overlays/packages.nix
@@ -1,5 +1,8 @@
 final: prev: {
   tiktoken = (prev.callPackage ../packages/tiktoken/tiktoken.nix { });
   llmcat = (prev.callPackage ../packages/llmcat/llmcat.nix { });
-  llm-mlx = (prev.callPackage ../packages/llm-mlx/default.nix { });
+  llm-mlx = (prev.callPackage ../packages/llm-mlx/llm-mlx.nix { });
+  nomicfoundation-solidity-language-server = (prev.callPackage
+    ../packages/nomicfoundation-solidity-language-server/nomicfoundation-solidity-language-server.nix
+    { });
 }
