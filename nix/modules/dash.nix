37ec573fbaaccf9fe9112e67ef6d471a94283a92
Adriano Di Luzio <aldur@users.noreply.github.com>
2025-10-29 09:07:37 +0100

    Fix $DASHT_DOCSETS_DIR for macOS

--- nix/modules/dash.nix
+++ nix/modules/dash.nix
@@ -1,25 +1,35 @@
 { pkgs, lib, ... }:
 with lib;
 let
-  basePackages = with pkgs; [ dasht dashp w3m ];
+  basePackages = with pkgs; [
+    dasht
+    dashp
+    w3m
+  ];
 
   dashDocsetsDir = ".local/share/dasht/docsets";
 
   download-nix-docsets = pkgs.writeShellApplication {
     name = "download-nix-docsets";
-    runtimeInputs = with pkgs; [ curl gnutar ];
+    runtimeInputs = with pkgs; [
+      curl
+      gnutar
+    ];
     text = ''
       curl https://aldur.github.io/nixpkgs.docset/all.tgz | tar --overwrite -xzf - -C "$DASHT_DOCSETS_DIR"
     '';
   };
-in {
+in
+{
   environment.systemPackages = basePackages;
 
-  home-manager.users.aldur = { ... }: {
-    home.file."${dashDocsetsDir}/.keep".text = "";
-    home.sessionVariables = {
-      DASHT_DOCSETS_DIR = "/home/aldur/${dashDocsetsDir}";
+  home-manager.users.aldur =
+    { ... }:
+    {
+      home.file."${dashDocsetsDir}/.keep".text = "";
+      home.sessionVariables = {
+        DASHT_DOCSETS_DIR = "$HOME/${dashDocsetsDir}";
+      };
+      home.packages = [ download-nix-docsets ];
     };
-    home.packages = [ download-nix-docsets ];
-  };
 }
