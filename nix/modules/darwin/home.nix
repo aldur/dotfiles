f10db9950e7dfcd34f63479d4ecac4b8ee7f6d70
Adriano Di Luzio <aldur@users.noreply.github.com>
2025-11-01 20:20:45 +0100

    Add macOS aliases

--- nix/modules/darwin/home.nix
+++ nix/modules/darwin/home.nix
@@ -1,10 +1,17 @@
 # macOS-specific home-manager configuration
-{ config, ... }:
-{
+{ config, ... }: {
   imports = [ ../home/home.nix ];
 
   home.homeDirectory = "/Users/${config.home.username}";
 
   # Silence "Last login: ..."
   home.file.".hushlogin".text = "";
+
+  home.shellAliases = {
+    tailscale = "/Applications/Tailscale.app/Contents/MacOS/Tailscale";
+
+    faraday = "sandbox-exec -p '(version 1)(allow default)(deny network*)'";
+    sandbox =
+      "sandbox-exec -p '(version 1)(allow default)(deny network*)(deny file-read-data (regex \"^/Users/'$USER'/(Documents|Desktop|Developer|Movies|Music|Pictures)\"))'";
+  };
 }
