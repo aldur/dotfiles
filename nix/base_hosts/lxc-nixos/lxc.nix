fbf04ca9cdb7e361e4c35a5973eaf32836828ac4
Adriano Di Luzio <aldur@users.noreply.github.com>
2025-11-03 09:04:12 +0100

    Style nits

--- nix/base_hosts/lxc-nixos/lxc.nix
+++ nix/base_hosts/lxc-nixos/lxc.nix
@@ -1,4 +1,10 @@
-{ pkgs, config, inputs, ... }: {
+{
+  pkgs,
+  config,
+  inputs,
+  ...
+}:
+{
   imports = [
     "${inputs.self}/modules/current_system_flake.nix"
     inputs.nixos-crostini.nixosModules.default
@@ -7,11 +13,21 @@
   networking.hostName = "lxc-nixos";
   hardware.graphics.enable = true;
 
-  programs.aldur.lazyvim.enable = true;
-  programs.aldur.lazyvim.packageNames = [ "lazyvim" ];
+  programs = {
+
+    aldur.lazyvim.enable = true;
+    aldur.lazyvim.packageNames = [ "lazyvim" ];
+    gnupg.agent.pinentryPackage = pkgs.pinentry-qt;
+
+    # This configures default SSH connections (including those initiated by
+    # root when using a remote builder) to go through yubikey-agent.
+    ssh.extraConfig = ''
+      Host *
+        IdentityAgent /run/user/1000/yubikey-agent/yubikey-agent.sock
+    '';
+  };
 
   services.yubikey-agent.enable = true;
-  programs.gnupg.agent.pinentryPackage = pkgs.pinentry-qt;
 
   # This makes it so that `sommelier` can set `DISPLAY`,
   # which is then used by `pinentry`.
@@ -24,13 +40,6 @@
 
   # Make it possible to use remote builders under this username
   nix.settings.trusted-users = [ config.users.users.aldur.name ];
-
-  # This configures default SSH connections (including those initiated by
-  # root when using a remote builder) to go through yubikey-agent.
-  programs.ssh.extraConfig = ''
-    Host *
-      IdentityAgent /run/user/1000/yubikey-agent/yubikey-agent.sock
-  '';
 
   environment.etc = {
     "ssh/ssh_host_ed25519_key" = {
@@ -48,22 +57,29 @@
 
   # NOTE: There a bug (maybe) in pcscd where, when running in an lxc container,
   # it doesn't automatically exit when the "smart card" is disconnected.
-  # 
+  #
   # When a new smart card is connected (i.e., the security key is re-attached
-  # to the container), it will fail to detect it and the SSH agent won't 
+  # to the container), it will fail to detect it and the SSH agent won't
   # work. The fix is easy: you just need to restart pcscd. But it requires
   # sudo privileges and the `aldur` user has no password.
-  security.sudo-rs.extraRules = [{
-    users = [ "aldur" ];
-    commands = [{
-      command = "/run/current-system/sw/bin/systemctl restart pcscd.service";
-      options = [ "NOPASSWD" ];
-    }];
-  }];
+  security.sudo-rs.extraRules = [
+    {
+      users = [ "aldur" ];
+      commands = [
+        {
+          command = "/run/current-system/sw/bin/systemctl restart pcscd.service";
+          options = [ "NOPASSWD" ];
+        }
+      ];
+    }
+  ];
 
   home-manager.users.aldur =
     { config, ... }: # home-manager's config, not the OS one
     {
-      home.packages = with pkgs; [ age-plugin-yubikey yubikey-manager ];
+      home.packages = with pkgs; [
+        age-plugin-yubikey
+        yubikey-manager
+      ];
     };
 }
