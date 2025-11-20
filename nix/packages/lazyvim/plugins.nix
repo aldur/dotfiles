d85f03a4392837623a8745c550de38904e97c871
aldur <aldur@users.noreply.github.com>
2025-11-20 09:15:55 +0100

    Bump clarity.nvim

--- nix/packages/lazyvim/plugins.nix
+++ nix/packages/lazyvim/plugins.nix
@@ -76,7 +76,7 @@
     }
 
     {
-      plugin = (pkgs.vimUtils.buildVimPlugin rec {
+      plugin = pkgs.vimUtils.buildVimPlugin rec {
         name = "tinymd.nvim";
         src = pkgs.fetchFromGitHub {
           owner = "aldur";
@@ -84,12 +84,12 @@
           rev = "f1caf374827de0e01a7bc90bdb6761fcbfab3b1f";
           hash = "sha256-Sl+L3fQMs/YsVllDuJpmwFNGtaDeta5okH3Kl5+xI1g=";
         };
-      });
+      };
       name = "tinymd.nvim";
     }
 
     {
-      plugin = (pkgs.symlinkJoin {
+      plugin = pkgs.symlinkJoin {
         name = "clarity.nvim_treesitter";
         paths = [
           (pkgs.vimUtils.buildVimPlugin {
@@ -97,28 +97,30 @@
             src = pkgs.fetchFromGitHub {
               owner = "aldur";
               repo = "clarity.nvim";
-              rev = "727318ce294f96713a2d093d253e0704d729be74";
-              hash = "sha256-6U0rvT76O5qO/0eQPUGoCqcddaQ5L6aKzMaddPmy6TI=";
+              rev = "be621c9902ab7d897577ba17d74dcb9fa1ee66bc";
+              hash = "sha256-6yzQk67VTpjUY33PA8DBLKDc+lDn/Z7gp4jH3AELass=";
             };
             doCheck = false; # Missing runtime dependencies for "require" check
           })
-          (pkgs.neovimUtils.grammarToPlugin (pkgs.tree-sitter.buildGrammar rec {
-            language = "clarity";
-            version = "ef0552d593a64d6d2936090a44f0ad9f5d54a37f";
-            src = pkgs.fetchFromGitHub {
-              owner = "xlittlerag";
-              repo = "tree-sitter-${language}";
-              rev = version;
-              hash = "sha256-xBNJuT3d6GN+ocbtdJE1XyosMHwDMnRFIrScPr5pzwc=";
-            };
-          }))
+          (pkgs.neovimUtils.grammarToPlugin (
+            pkgs.tree-sitter.buildGrammar rec {
+              language = "clarity";
+              version = "cbb3ffe8688aca558286fd45ed46857a1f3207bb";
+              src = pkgs.fetchFromGitHub {
+                owner = "xlittlerag";
+                repo = "tree-sitter-${language}";
+                rev = version;
+                hash = "sha256-iylkAIBEpMPzRYHXyFQKMIEZJbqij/8tLdq9z/UPgN8=";
+              };
+            }
+          ))
         ];
-      });
+      };
       name = "clarity.nvim";
     }
 
     {
-      plugin = ((pkgs.vimUtils.buildVimPlugin {
+      plugin = pkgs.vimUtils.buildVimPlugin {
         name = "link.vim";
         src = pkgs.fetchFromGitHub {
           owner = "qadzek";
@@ -126,12 +128,12 @@
           rev = "0acbf748ae052edf0bd4d70a632a1bb289e1eb33";
           hash = "sha256-1Eq2arCC5dYDLCk5P2y3Gl1vv1TB3lpq56kJZNCQ7sI=";
         };
-      }));
+      };
       name = "link.vim";
     }
 
     {
-      plugin = (pkgs.vimUtils.buildVimPlugin rec {
+      plugin = pkgs.vimUtils.buildVimPlugin rec {
         name = "venv-selector.nvim";
         src = pkgs.fetchFromGitHub {
           owner = "linux-cultist";
@@ -139,7 +141,7 @@
           rev = "2b49d1f8b8fcf5cfbd0913136f48f118225cca5d";
           hash = "sha256-mz9RT1foan2DCHTZppuPZHaEqREqOHg2WU7uk3bjl0E=";
         };
-      });
+      };
       name = "venv-selector.nvim";
     }
   ];
