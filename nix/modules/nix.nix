9d06f49954be06d1e30b3b9fbdff9540ad816eb0
aldur <aldur@users.noreply.github.com>
2026-01-05 14:26:42 +0100

    Correctly pin `nixpkgs` registry

--- nix/modules/nix.nix
+++ nix/modules/nix.nix
@@ -26,12 +26,32 @@ in
         optimise = {
           automatic = true;
         };
-
-        # Pin nixpkgs to the flake input, so that the packages installed
-        # come from the flake inputs.nixpkgs.url.
-        registry.nixpkgs.flake = inputs.nixpkgs;
       };
     }
+
+    # Pin nixpkgs to the flake input.
+    (
+      let
+        inherit (pkgs.stdenv) isDarwin;
+
+        # `nix-darwin` 25.11+ does this automatically via nixpkgs.flake.setFlakeRegistry,
+        # so we only set it manually on Linux or if that option is disabled.
+        darwinFlakeRegistryDisabled =
+          isDarwin
+          && config ? nixpkgs
+          && config.nixpkgs ? flake
+          && config.nixpkgs.flake ? setFlakeRegistry
+          && !config.nixpkgs.flake.setFlakeRegistry;
+        shouldSetRegistry = pkgs.stdenv.isLinux || darwinFlakeRegistryDisabled;
+      in
+      lib.mkIf shouldSetRegistry {
+        nix.registry.nixpkgs.flake = inputs.nixpkgs;
+        warnings = lib.optionals darwinFlakeRegistryDisabled [
+          "nixpkgs.flake.setFlakeRegistry is disabled; falling back to manual nix.registry.nixpkgs configuration from dotfiles."
+        ];
+      }
+    )
+
     (lib.mkIf cfg.enable {
       nix.package = lib.mkForce inputs.detnix.packages."${pkgs.stdenv.hostPlatform.system}".default;
       # https://docs.determinate.systems/guides/telemetry
