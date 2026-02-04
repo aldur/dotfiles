bc897eb5bc36b9f5fe149247ff94a4f83092ac19
Adriano Di Luzio <aldur@users.noreply.github.com>
2026-02-04 06:08:52 +0100

    Disable `mlx-lm` tests

--- nix/overlays/overrides/llm.nix
+++ nix/overlays/overrides/llm.nix
@@ -11,18 +11,12 @@ final: prev: {
           hash = "sha256-H3LwMx3QFuU6d+ayIN2N+Y3Euv4L09oH0JUZV2Gd7Qw=";
         };
 
-        disabledTestPaths =
-          builtins.map (
-            path:
-            if path == "tests/test_utils_load_model.py" then
-              "tests/test_utils.py::TestUtils::test_load_model_with_custom_get_classes"
-            else
-              path
-          ) oldAttrs.disabledTestPaths
-          ++ [
-            "tests/test_models.py::TestModels::test_gated_delta"
-            "tests/test_models.py::TestModels::test_gated_delta_masked"
-          ];
+        # Do not seem to work reliably on GH CI
+        doCheck = false;
+
+        pythonImportsCheck = [
+          "mlx_lm"
+        ];
       });
       mlx = prev.callPackage ../../packages/mlx/default.nix {
         inherit (pythonFinal) buildPythonPackage fetchPypi;
