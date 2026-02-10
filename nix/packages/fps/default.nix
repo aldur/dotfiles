b83c1398745c8bbbd7a4ea0209c2afc8f162a393
Adriano Di Luzio <aldur@users.noreply.github.com>
2026-02-10 11:10:56 +0100

    Send `KILL` in `fps`

--- nix/packages/fps/default.nix
+++ nix/packages/fps/default.nix
@@ -13,6 +13,6 @@ writeShellApplication {
   ];
 
   text = ''
-    ps -eo pid,user,%cpu,%mem,args | fzf --header-lines=1 --footer "<c-k> kills PID; <cr> prints PID" --bind 'ctrl-k:execute(kill {1})+abort,enter:become(echo {1})'
+    ps -eo pid,user,%cpu,%mem,args | fzf --header-lines=1 --footer "<c-k> kills PID; <cr> prints PID" --bind 'ctrl-k:execute(kill -KILL {1})+abort,enter:become(echo {1})'
   '';
 }
