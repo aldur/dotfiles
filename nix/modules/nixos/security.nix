fada57431623104362ccaca96056fa05644d0d60
aldur <aldur@users.noreply.github.com>
2026-04-30 21:51:25 +0200

    Mitigate against CVE-2026-31431

--- nix/modules/nixos/security.nix
+++ nix/modules/nixos/security.nix
@@ -8,4 +8,14 @@
       execWheelOnly = true;
     };
   };
+
+  # --- Mitigation against CVE-2026-31431/copy.fail ---
+  boot.blacklistedKernelModules = [
+    "algif_aead"
+  ];
+
+  boot.extraModprobeConfig = ''
+    install algif_aead /bin/false
+  '';
+  # --- /Mitigation against CVE-2026-31431/copy.fail ---
 }
