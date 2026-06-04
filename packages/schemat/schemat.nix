e0cf885fff6a897457d85a9e7388ecab85438a4a
aldur <aldur@users.noreply.github.com>
2026-06-04 15:01:43 +0200

    Bump schemeat

--- packages/schemat/schemat.nix
+++ packages/schemat/schemat.nix
@@ -3,7 +3,7 @@
 }:
 with import <nixpkgs> {
   overlays = [
-    (import (fetchTarball "https://github.com/oxalica/rust-overlay/archive/954582a766a50ebef5695a9616c93b5386418c08.tar.gz"))
+    (import (fetchTarball "https://github.com/oxalica/rust-overlay/archive/c30ca201c5093540cf792f6982f81ba1aa0f3514.tar.gz"))
   ];
 };
 let
@@ -14,14 +14,13 @@ let
 in
 rustPlatform.buildRustPackage rec {
   name = "schemat";
-  version = "v0.1.23";
+  version = "v0.5.2";
   src = fetchFromGitHub {
     owner = "raviqqe";
     repo = name;
     rev = version;
-    hash = "sha256-k5Y+Whb0Z2wlEjZOqcf9ex930M+FBokh0Ewt1l1JBoM=";
+    hash = "sha256-Ij7JigbXhE2o0Z61uZ3W/pK7zcQyrX+SMpF0iKsVx30=";
   };
 
-  useFetchCargoVendor = true;
-  cargoHash = "sha256-niHpdA5HfIn0fWFxlKUTSVR/P+lfHHSVGr54Y8nqxcY=";
+  cargoHash = "sha256-oaET2IGU78TUC98HKsiQnbg7R262ugrn8oiLeKC767s=";
 }
