bf9eda069609dfe0633274de6f50c03b5c23b5a3
aldur <aldur@users.noreply.github.com>
2026-07-03 11:23:24 +0200

    Add note to pragmatism

--- modules/nixos/pragmatism.nix
+++ modules/nixos/pragmatism.nix
@@ -1,5 +1,10 @@
 { pkgs, ... }: {
   # https://fzakaria.com/2025/02/26/nix-pragmatism-nix-ld-and-envfs
+  #
+  # Deliberately trades some of hardening.nix's attack-surface reduction for
+  # foreign-binary compatibility: nix-ld runs arbitrary dynamically-linked
+  # binaries and envfs (FUSE) auto-populates /usr/bin. Opt-in per host; don't
+  # cargo-cult onto a locked-down box.
   programs = {
     nix-ld = {
       enable = true;
