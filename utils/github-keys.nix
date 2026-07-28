d19a3a9ccd26fd87baabc1769c27c4507e88d0ba
aldur <aldur@users.noreply.github.com>
2026-07-28 10:06:09 +0000

    Bump github-keys hash

--- utils/github-keys.nix
+++ utils/github-keys.nix
@@ -1,6 +1,6 @@
 {
   username ? "aldur",
-  sha256 ? "sha256-fX18uyZHlf1Sp+RpCAUiCSP2VZf9gGdHZfqWV33b5W0=",
+  sha256 ? "sha256-NIeF0Y/UzSA3mgy8geh6XOrsckgqWvwAvuclZihhEK4=",
 }:
 # NOTE: This uses only `builtins` to be independent of nixpkgs/system
 let
