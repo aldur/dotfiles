5d400f4e39cc2bfbbfb17d30c2290be71e85f251
aldur <aldur@users.noreply.github.com>
2026-08-05 19:03:23 +0200

    Call `beans format` instead of LSP if it exists

--- packages/lazyvim/lua/plugins/conform.lua
+++ packages/lazyvim/lua/plugins/conform.lua
@@ -7,12 +7,20 @@ return {
 			opts.formatters_by_ft["markdown"] = opts.formatters_by_ft["markdown"] or {}
 			table.insert(opts.formatters_by_ft["markdown"], "trim_whitespace")
 
-			opts.formatters_by_ft["beancount"] = opts.formatters_by_ft["beancount"] or {}
-			table.insert(opts.formatters_by_ft["beancount"], "bean-format")
-			opts.formatters_by_ft["beancount"]["lsp_format"] = "fallback"
+			opts.formatters_by_ft["beancount"] = {
+				"beans",
+				"bean-format",
+				stop_after_first = true,
+				lsp_format = "fallback",
+			}
 
 			opts.formatters = opts.formatters or {}
 			opts.formatters["bean-format"] = { prepend_args = { "-c", "70" } }
+			opts.formatters["beans"] = {
+				command = "beans",
+				args = { "format", "--stdin" },
+				stdin = true,
+			}
 		end,
 	},
 }
