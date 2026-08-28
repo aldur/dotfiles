82a6480e1f5e38db647d535dfc7447c035f3837f
aldur <aldur@users.noreply.github.com>
2026-08-28 11:45:39 +0200

    Bump clarity-nvim plugin

--- packages/lazyvim/plugins/clarity-nvim.nix
+++ packages/lazyvim/plugins/clarity-nvim.nix
@@ -5,13 +5,13 @@
 
 vimUtils.buildVimPlugin {
   pname = "clarity.nvim";
-  version = "0.3.0-unstable-2026-07-28";
+  version = "0.3.0-unstable-2026-08-28";
 
   src = fetchFromGitHub {
     owner = "aldur";
     repo = "clarity.nvim";
-    rev = "5c9d8accc29e0262fd9fb2013e1dc45b01bcba1e";
-    hash = "sha256-EGrC3wY+WCOykYMJnPqeK6IrcGzByIe7hP8ktdXA2sI=";
+    rev = "df3f2decf4dafa24f70e3e51388e4780182496b0";
+    hash = "sha256-27IIR4OujcRwqtURkZ2K9NgEzdf9X59L7YDikH5Cd5I=";
   };
 
   doCheck = false; # Missing runtime dependencies for "require" check
