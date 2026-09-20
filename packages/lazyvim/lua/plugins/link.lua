4016564068dc74fe7b3418743d25bc042ae5de10
aldur <aldur@users.noreply.github.com>
2026-09-20 18:19:24 +0200

    Use lazyvim's ft instead of `vim.g.link_enabled_filetypes`

--- packages/lazyvim/lua/plugins/link.lua
+++ packages/lazyvim/lua/plugins/link.lua
@@ -1,8 +1,8 @@
 return {
 	{
 		"qadzek/link.vim",
+		ft = { "markdown", "gitcommit" },
 		init = function()
-			vim.g.link_enabled_filetypes = { "markdown", "gitcommit" }
 			vim.g.link_heading = ""
 			vim.g.link_disable_internal_links = 1
 		end,
